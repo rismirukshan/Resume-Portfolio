af19635622bf4775aeb769d7cb00057fa4ab52f0
Rismi Rukshan <rukshanlancer@gmail.com>
2022-10-27 07:46:28 -0600

    banner part work

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mantine/core";
 import {Menu} from "../Menu";
 export const Banner = () => {
     return(
@@ -5,7 +6,17 @@ export const Banner = () => {
         <section id='banner'>
             <Menu icon="profile.png" title1="Welcome to" title2="my profile"></Menu>
             <div>
-                <h1>banner part</h1>
+                <Grid>
+                    <Grid.Col xs={6}>
+                        <img src={`/images/profile.png`} style={{width: "-webkit-fill-available"}} alt="profile of portfolio"/>
+                    </Grid.Col>
+                    <Grid.Col xs={6}>
+                        <div>
+                            <h2>I am Rismi.</h2>
+                            <span>Web Developer & Web Designer</span>
+                        </div>
+                    </Grid.Col>
+                </Grid>
             </div>
         </section>
         </>
